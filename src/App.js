5a842bec2d1f0a94db592310d015a51abc8af13d
John Genere <johncgenere@gmail.com>
2019-06-04 12:04:22 -0400

    Added state variables for first and last name with edit button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component{
+  constructor(props){
+      super(props);
+
+      this.state = {
+        firstName: 'John',
+        lastName: 'Smith'
+      }
+  }
+
+
+
   render(){
     return (
       <div className="App">
         <header className="App-header">
-          <h1> Hello World! </h1>
+        <form className="ui large form">
+          <div className="ui stacked segment">
+            <h2 style={{color: 'black'}}> First Name: {this.state.firstName} </h2>
+            <h2 style={{color: 'black'}}> Last Name: {this.state.lastName} </h2>
+          </div>
+          <div className="ui fluid large black submit button" onClick={this.handleEdit}>Edit</div>
+        </form>
         </header>
       </div>
     );
